2a7cd51e7792a7ddb90fa585a956c4e22483172e
ParkJiHyeon <91263647+parkjihyeon124594@users.noreply.github.com>
2024-04-14 22:21:47 +0900

    feat : 메세지 DB 저장

--- src/main/java/com/example/OneToOneChat/websocket/controller/WebSocketChatController.java
+++ src/main/java/com/example/OneToOneChat/websocket/controller/WebSocketChatController.java
@@ -8,6 +8,7 @@ import com.example.OneToOneChat.domain.chatMessage.service.ChatMessageService;
 import com.example.OneToOneChat.domain.chatRomm.dto.Request.ChatMessageCreateRequest;
 import com.example.OneToOneChat.domain.chatRomm.entity.ChatMessage;
 import com.example.OneToOneChat.domain.chatRomm.repository.ChatMessageRepository;
+import com.example.OneToOneChat.global.config.ServerEndpointConfigurator;
 import com.example.OneToOneChat.global.exception.GlobalException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.websocket.*;
@@ -22,13 +23,26 @@ import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Service
-@ServerEndpoint("/socket/chatt")
+@ServerEndpoint(value = "/socket/chatt",configurator = ServerEndpointConfigurator.class)
 @Slf4j
+@RequiredArgsConstructor
 public class WebSocketChatController {
 
+    private static final int MAX_CLIENTS = 3;
+    private final ChatMessageService chatMessageService;
     private static List<Session> clients = Collections.synchronizedList(new ArrayList<Session>());
     @OnOpen
     public void onOpen(Session session) throws IOException {
+ /*       if (clients.size() >= MAX_CLIENTS) {
+            // 최대 연결 수 초과 시 기존 2개의 연결 제거
+            for (int i = 0; i < 2; i++) {
+                Session removeSession = clients.get(i);
+                clients.remove(removeSession);
+                log.info("최대 연결 수 초과! 기존 세션 제거 : {}", removeSession);
+            }
+        }
+*/
+        sendSessionCountToAllClients();
 
         if (!clients.contains(session)) {
             clients.add(session);
@@ -37,8 +51,22 @@ public class WebSocketChatController {
             log.info("이미 연결된 session");
         }
         log.info("open session : {}, clients={}", session.toString(), clients);
+
+        log.info("현재 접속 중인 세션의 size{}",clients.size());
+
     }
-   /* @OnMessage
+
+    private void sendSessionCountToAllClients() throws IOException {
+        // 현재 세션 크기 정보를 담은 JSON 객체 생성
+        Map<String, Object> message = new HashMap<>();
+        message.put("connectedClients", clients.size());
+
+        // 모든 클라이언트에게 JSON 객체 전송
+        for (Session s : clients) {
+            s.getBasicRemote().sendText(new ObjectMapper().writeValueAsString(message));
+        }
+    }
+    @OnMessage
     public void onMessage(String message, Session session) throws IOException {
         log.info("receive message : {}", message);
 
@@ -53,35 +81,16 @@ public class WebSocketChatController {
         String writer = (String) messageMap.get("writer");
         String content = (String) messageMap.get("content");
         Long roomId =  Long.parseLong((String) messageMap.get("roomId"));
+        log.info("room id test {}",roomId);
 
         ChatMessageCreateRequest chatMessageCreateRequest = ChatMessageCreateRequest.builder()
                 .content(content)
                 .writer(writer).build();
 
-*//*        ChatRoom findChatRoom= chatRoomRepository.findById(roomId)
-                .orElseThrow(() -> new GlobalException(ChatRoomError.CHAT_ROOM_NOT_FOUND));
+        chatMessageService.saveMessage(chatMessageCreateRequest,roomId);
 
-
-        ChatMessage chatMessage = ChatMessage.builder()
-                .writer(writer)
-                .content(content)
-                .chatRoom(findChatRoom)
-                .build();
-        chatMessageRepository.save(chatMessage);*//*
-
-
-    }*/
-
-    @OnMessage
-    public void onMessage(String message, Session session) throws IOException {
-        log.info("receive message : {}", message);
-
-        for (Session s : clients) {
-            log.info("send data : {}", message);
-
-            s.getBasicRemote().sendText(message);
-        }
     }
+
 
     @OnClose
     public void onClose(Session session) {
